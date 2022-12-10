23802a4b0ca177e88b26a82334fbcb062508ee94
unknown <you@example.com>
2022-12-10 07:12:44 +0530

    Added : retrieve  employee payroll data from JSON server

--- homePage.js
+++ homePage.js
@@ -1,14 +1,33 @@
 let empPayrollList;
 window.addEventListener('DOMContentLoaded', (event) => {
-    empPayrollList = getEmployeePayrollDataFromStorage();
+    if(site_properties.use_local_storage.match("true")){
+    getEmployeePayrollDataFromStorage();
+    }
+    else getEmployeePayrollDataFromServer();
+});
+
+const processEmployeePayrollDataResponse = () => {
     document.querySelector(".emp-count").textContent = empPayrollList.length;
     createInnerHtml();
     localStorage.removeItem('editEmp');
-});
+}
 
+const getEmployeePayrollDataFromServer = () => {
+    makeServiceCall("GET", site_properties.server_url, true)
+    .then(reponseText => {
+        empPayrollList = JSON.parse(responseText);
+        processEmployeePayrollDataResponse();
+    })
+    .catch(error => {
+        console.log("GET Error Status: " +JSON.stringify(error));
+        empPayrollList = [];
+        processEmployeePayrollDataResponse();
+    });
+}
 const getEmployeePayrollDataFromStorage = () => {
-    return localStorage.getItem('EmployeePayrollList') ?
-        JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+    empPayrollList = localStorage.getItem('EmployeePayrollList') ?
+                        JSON.parse(localStorage.getItem('EmployeePayrollList')):[];
+    processEmployeePayrollDataResponse();
 }
 
 const createInnerHtml = () => {
